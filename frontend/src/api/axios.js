858042b3c00138bfed9ca2d585aa6de7d5226117
pranayrauni <ipranaysingh@gmail.com>
2025-06-24 23:06:49 +0530

    add backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import store from '../store'
 
-const API = 'rbac-production-ccbf.up.railway.app/api';
+const api_local = false
+const API = api_local ? 'http://localhost:5000/' : 'https://rbac-production-ccbf.up.railway.app/api';
 const instance = axios.create({
   baseURL: API
 })
